62401a7556d0cf4f25f6300397f8456f4ff3da06
Jochen Klein <jochen.klein@cern.ch>
2020-04-02 16:36:44 +0200

    Fix permissions

--- macOS/bootstrap_macos.sh
+++ macOS/bootstrap_macos.sh
@@ -12,6 +12,7 @@ brew install ansible git
 [[ -d ${HOME}/sysadmin ]] || mkdir ${HOME}/sysadmin
 cd ${HOME}/sysadmin
 curl -fsSLO -u qgp https://dl.qgp.io/deploy/deploykey_id_ed25519
+chmod 0600 deploykey_id_ed25519
 git clone git@github.com:qgp/qgp-playbook -c core.sshCommand="ssh -o IdentityFile=deploykey_id_ed25519" -o qgp
 
 # ansible-pull -U git@github.com:qgp/qgp-playbook.git -d ${HOME}/sysadmin/qgp-playbook --check
