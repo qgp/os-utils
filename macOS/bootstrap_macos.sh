02d1b9f867ac3ca30a1a8cfb0f466c1a59546e83
Jochen Klein <jochen.klein@cern.ch>
2020-04-02 15:52:29 +0200

    Fix download

--- macOS/bootstrap_macos.sh
+++ macOS/bootstrap_macos.sh
@@ -11,7 +11,7 @@ brew install ansible git
 # clone ansible repo
 [[ -d ${HOME}/sysadmin ]] || mkdir ${HOME}/sysadmin
 cd ${HOME}/sysadmin
-curl -fsSLO -u qgp https://dl.qgp.io/deploy/id_25519_deploy
-git clone git@github.com:qgp/qgp-playbook -c core.sshCommand="ssh -o IdentifyFile=id_25519_deploy" -o qgp
+curl -fsSLO -u qgp https://dl.qgp.io/deploy/deploykey_id_ed25519
+git clone git@github.com:qgp/qgp-playbook -c core.sshCommand="ssh -o IdentityFile=deploykey_id_ed25519" -o qgp
 
 # ansible-pull -U git@github.com:qgp/qgp-playbook.git -d ${HOME}/sysadmin/qgp-playbook --check
