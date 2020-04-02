535c128ab3bce79598e36a0f05d4394dae38b6d6
Jochen Klein <jochen.klein@cern.ch>
2020-04-02 15:45:57 +0200

    Fix curl

--- macOS/bootstrap_macos.sh
+++ macOS/bootstrap_macos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# first install brew (used also for installation of ansible then)
-# (this also installs Xcode command line tools)
+# first install brew
+# (pulling in Xcode command line tools)
 brew --version > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install ansible and git
@@ -11,7 +11,7 @@ brew install ansible git
 # clone ansible repo
 [[ -d ${HOME}/sysadmin ]] || mkdir ${HOME}/sysadmin
 cd ${HOME}/sysadmin
-curl -fsSLO https://dl.qgp.io/deploy/id_25519_deploy
+curl -fsSLO -u qgp https://dl.qgp.io/deploy/id_25519_deploy
 git clone git@github.com:qgp/qgp-playbook -c core.sshCommand="ssh -o IdentifyFile=id_25519_deploy" -o qgp
 
 # ansible-pull -U git@github.com:qgp/qgp-playbook.git -d ${HOME}/sysadmin/qgp-playbook --check
