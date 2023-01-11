73500c165fa6d1530a2e99402f34374e1cb48549
Jochen Klein <jochen.klein@cern.ch>
2023-01-11 12:42:45 +0100

    Update for jklma

--- macOS/bootstrap_macos.sh
+++ macOS/bootstrap_macos.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-# first install brew
+# first install homebrew
 # (pulling in Xcode command line tools)
-brew --version > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+brew --version > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install ansible and git
 brew install ansible git
 
 # clone ansible repo
-[[ -d ${HOME}/sysadmin ]] || mkdir ${HOME}/sysadmin
-cd ${HOME}/sysadmin
+[[ -d ${HOME}/Admin]] || mkdir ${HOME}/Admin
+cd ${HOME}/Admin
 curl -fsSLO -u qgp https://dl.qgp.io/deploy/deploykey_id_ed25519
 chmod 0600 deploykey_id_ed25519
 git clone git@github.com:qgp/qgp-playbook -c core.sshCommand="ssh -o IdentityFile=deploykey_id_ed25519" -o qgp
